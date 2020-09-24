ca459607db8839534951b33d3ae90117be066025
pratap <pratapdeb1996@gmail.com>
2020-09-24 22:36:19 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Link from './component/Link'
+function App() {
+  return (
+    <div className="App">
+     <Link/>
+    </div>
+  );
+}
+
+export default App;
